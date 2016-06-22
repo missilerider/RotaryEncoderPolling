bf7c8f0b52282ffde75c6e293f7902a5d175f79f
missilerider <alfonso.vila.lopez@gmail.com>
2016-06-23 00:26:16 +0200

    Create RotaryEncoderPolling.cpp

---
+++ RotaryEncoderPolling/RotaryEncoderPolling.cpp
@@ -0,0 +1,104 @@
+#include <Arduino.h>
+#include "RotaryEncoderPolling.h"
+
+void RotaryEncoder::setup(int pinA, int pinB)
+{
+	pin_A = pinA;
+	pin_B = pinB;
+	pin_C = 0;
+}
+
+void RotaryEncoder::setup(int pinA, int pinB, int pinC)
+{
+	pin_A = pinA;
+	pin_B = pinB;
+	pin_C = pinC;
+}
+
+void RotaryEncoder::prepare()
+{
+	pinMode(pin_A, INPUT);
+	digitalWrite(pin_A, HIGH);
+
+	pinMode(pin_B, INPUT);
+	digitalWrite(pin_B, HIGH);
+
+	if (pin_C > 0)
+	{
+		pinMode(pin_C, INPUT);
+		digitalWrite(pin_C, HIGH);
+	}
+}
+
+char RotaryEncoder::getInput(int MaxWait)
+{
+	long MaxMillis = millis() + MaxWait * 1000;
+
+	int dir_0, dir_1;
+
+	while (true)
+	{
+		dir_0 = digitalRead(pin_A);
+		dir_1 = digitalRead(pin_B);
+
+		while (dir_0 + dir_1 < 2) // No neutro
+		{
+			dir_0 = digitalRead(pin_A);
+			dir_1 = digitalRead(pin_B);
+		}
+
+		while (dir_0 + dir_1 == 2) // Neutro
+		{
+			dir_0 = digitalRead(pin_A);
+			dir_1 = digitalRead(pin_B);
+
+			if (pin_C > 0)
+				if (!digitalRead(pin_C))
+				{
+					while (!digitalRead(pin_C)) { delay(10); }
+					return 3;
+				}
+
+			if (MaxWait > -1)
+				if (MaxMillis < millis()) return -1; // TIMEOUT
+		}
+
+		if (dir_0 == 1 && dir_1 == 0)
+		{
+			dir_0 = digitalRead(pin_A);
+			dir_1 = digitalRead(pin_B);
+
+			while (dir_0 == 1 && dir_1 == 0)
+			{
+				dir_0 = digitalRead(pin_A);
+				dir_1 = digitalRead(pin_B);
+			}
+
+			if (dir_0 == 0 && dir_1 == 0)
+			{
+				return 1;
+			}
+		}
+		else if (dir_0 == 0 && dir_1 == 1)
+		{
+			dir_0 = digitalRead(pin_A);
+			dir_1 = digitalRead(pin_B);
+
+			while (dir_0 == 0 && dir_1 == 1)
+			{
+				dir_0 = digitalRead(pin_A);
+				dir_1 = digitalRead(pin_B);
+			}
+
+			if (dir_0 == 0 && dir_1 == 0)
+			{
+				return 2;
+			}
+		}
+	}
+}
+
+char RotaryEncoder::getInput()
+{
+	return getInput(-1);
+}
